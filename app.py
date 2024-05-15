5bce35bc2563b224b5e76c3fe015203aa03bef5e
todd edwards <yewtreestudios@hotmail.co.uk>
2024-05-15 22:24:05 +0000

    login page app routing added

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -16,19 +17,48 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
-
+# Home (index) page
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("index.html")
 
 
-
+# Reviews page
 @app.route("/get_reviews")
 def get_reviews():
     reviews = mongo.db.reviews.find()
-    # print("Reviews: ", reviews)
     return render_template("reviews.html", reviews=reviews)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        existing_user = mongo.db.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # check hashed password matches users password input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Hi {}!".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "create_review", username=session["user"]))
+            else:
+                # Password doesn't match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # Username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+            
+    return render_template("login.html")
+
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
