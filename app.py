fbfce71e6342e6b72a1fb956496227c328e73afc
todd edwards <yewtreestudios@hotmail.co.uk>
2024-05-23 09:43:36 +0000

    logout route redirected to url for index instead of login

--- app.py
+++ app.py
@@ -168,7 +168,7 @@ def logout():
     # remove user from session cookies
     flash("You have been logged out")
     session.pop("user")
-    return redirect(url_for("login"))
+    return redirect(url_for("index"))
 
 
 # Unauthorised access for users that
